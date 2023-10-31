f5dc5d1b3e343fe145ab529a5c7be7dbc0f1e558
김희수 <hs.kim@pia.space>
2023-10-31 18:29:28 +0900

    initial script

---
+++ main_format_conversion.py
@@ -0,0 +1,19 @@
+import argparse
+
+
+def get_args(description="Converts json file format"):
+    parser = argparse.ArgumentParser(description=description)
+    # TODO: Add input arguments
+    # parser.add_argument("--input_file", help="Input file path")
+    # parser.add_argument(...)
+
+
+def main():
+    args = get_args()
+    # TODO: Add your code here
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
